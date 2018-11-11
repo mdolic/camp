4b9e708f0f7bcd3cb9186b5696b64f007559122f
mdolic <mirzetdolic2@gmail.com>
2018-11-11 08:19:28 -0600

    updated

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const commentRoutes = require("./routes/comments"),
 
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true }); //localhost 3000 server
-let url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
-mongoose.connect(url); //mongolab database
+// let url = process.env.DATABASEURL || "mongodb://mdolic1:adaymia1@ds159273.mlab.com:59273/dkrogue"
+// mongoose.connect(url); //mongolab database
+
+mongoose.connect('mongodb://miki:amari1@ds159273.mlab.com:59273/dkrogue', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs"); //this just lets me set the view so its ejs and i dont ahve to use ejs below in renderr
